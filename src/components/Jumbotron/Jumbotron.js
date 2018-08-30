9106e7a9d7bbd7dea10fd77051f3c3db5dd9be6c
audrey <audreyfernandez@verizon.net>
2018-08-29 23:09:48 -0700

    do not merge

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -3,12 +3,13 @@ import "./Jumbotron.css";
 
 
 const Jumbotron = () => (
-<div className="jumbotron jumbotron-fluid">
+<div className="topHeader">
   <div className="container text-center">
-    <h1 className="display-4">Catcha Pokemon Game!</h1>
-    <p className="lead">Click on any card to catch a Pokemon, but don't click the same card twice or the Pokemon will get away!</p>
+    <span className="display-4">Catcha Pokemon Game!</span>
+    <p className="subTitle">Click on a card to catch one. Don't click the same card twice or it will get away!</p>
   </div>
 </div>
+
 );
 
 export default Jumbotron;
